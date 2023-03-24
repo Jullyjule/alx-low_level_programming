5f02eedb2324085278084cff30c5aad05ad173e9
juliana <Arowolo boluwatife>
2023-03-24 16:16:55 -0700

    print

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - using the function printf
+ *
+ * Return: 0 to be successful
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
